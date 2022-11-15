3543ba29de0ac2a0c138043a6700a02d22bcf19e
Tiago Pimentel <tiagomoreirapimentel@hotmail.com>
2022-11-15 17:13:06 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationBottun from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Olá mundo!</h1>
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationBottun />
+      <NotificationBottun />
+    </>
   )
 }
 export default App
